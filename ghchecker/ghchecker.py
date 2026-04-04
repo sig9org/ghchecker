b19df8f28a431bc606a654cdcf68f7ea050cfe5a
sig9org <sig9@sig9.org>
2026-04-04 23:38:43 +0900

    Initial commit: GitHub release checker with Webex notifications

---
+++ ghchecker/ghchecker.py
@@ -0,0 +1,47 @@
+import configparser
+import os
+
+from dotenv import load_dotenv
+from github import Github
+from notify_to_cisco_webex.notify_to_cisco_webex import Webex, WebexConfig
+
+
+def main():
+    gh = Github()
+    is_update = False
+    msg = "### GitHub New Release(s)\n\n"
+    cfg = configparser.ConfigParser()
+    cfg.read("config.ini")
+    for section in cfg.sections():
+        repo = cfg[section]["repo"]
+        oldver = cfg[section]["version"]
+
+        releases = gh.get_repo(repo).get_releases()
+        if releases.totalCount > 0:
+            newver = releases[0].tag_name
+            if oldver != newver:
+                is_update = True
+                msg += f"1. [{section}](https://github.com/{repo}) : [{oldver}](https://github.com/{repo}/releases/tag/{oldver}) ---> [{newver}](https://github.com/{repo}/releases/tag/{newver})\n"
+                cfg.set(section, "version", newver)
+
+    if is_update:
+        with open("config.ini", "w") as f:
+            cfg.write(f)
+        try:
+            load_dotenv()
+            wcfg = WebexConfig(
+                token=os.environ.get("WEBEX_TOKEN"),
+                dst=os.environ.get("WEBEX_DST"),
+                msg_format="markdown",
+                timeout=10.0,
+                insecure=False,
+                verbose=False,
+                proxy=None,
+            )
+            Webex(wcfg).send(message=msg)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
